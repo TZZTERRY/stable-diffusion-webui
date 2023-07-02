e059c1bc4f8002995d195ada9278bbf53fbbbe66
TerryZ <zt0432303734@gmail.com>
2023-07-02 18:18:43 +1000

    Deleted command line arguments (--update-check)

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON=C:\Users\ztaus\AppData\Local\Programs\Python\Python310\python.exe
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--autolaunch --xformers --allow-code --skip-install --update-check --skip-python-version-check
+set COMMANDLINE_ARGS=--autolaunch --xformers --allow-code --skip-install --skip-python-version-check
 
 call webui.bat
